24f59d961121ca8e14340e00265e5d0fc841ba14
izzunnafis <izzun1412nafis@gmail.com>
2024-07-18 14:53:23 +0700

    add custom cuda path

--- visualDet3D/networks/lib/ops/dcn/make.sh
+++ visualDet3D/networks/lib/ops/dcn/make.sh
@@ -1,5 +1,5 @@
-export CUDA_HOME=/usr/local/cuda
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda
+export CUDA_HOME=/usr/local/cuda        #"/home/rispro-sils/ADAS_Kedaireka/env/cuda-11.8/"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda #"/home/rispro-sils/ADAS_Kedaireka/env/cuda-11.8/"
 export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
 export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH"
 export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
